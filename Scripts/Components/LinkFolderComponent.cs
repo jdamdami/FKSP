44ba2041cfd665a1dac45cfc08124a24bd46601b
jdamdami <66435927+jdamdami@users.noreply.github.com>
2025-09-13 22:28:48 +0200

    Link assets WIP

--- Scripts/Components/LinkFolderComponent.cs
+++ Scripts/Components/LinkFolderComponent.cs
@@ -4,14 +4,35 @@ using System;
 public partial class LinkFolderComponent : Node
 {
     [Export] private FileDialog _fileDialog;
+    [Export] private Button _linkNewFolderButton;
+    [Export] private HBoxContainer _thunmnailsBoxContainer;
+    
 
-    public override void _Ready()
+
+public override void _Ready()
     {
         _fileDialog.FilesSelected += OnNewFilesSelected;
+
+        _linkNewFolderButton.Pressed += OnLinkNewFolderButtonPressed;
     }
+
+    private void OnLinkNewFolderButtonPressed()
+    {
+        _fileDialog.PopupCentered(new Vector2I(800, 600));
+    }
+
 
     private void OnNewFilesSelected(string[] paths)
     {
+        GD.Print("OnNewFilesSelected");
+
+        foreach (string path in paths)
+        {
+            GD.Print("Importing asset : " + path);
+            
+            
+        }
+        
         
     }
 }
