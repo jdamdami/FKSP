865a91ee54669b2c3daa59778456131393059c38
jdamdami <66435927+jdamdami@users.noreply.github.com>
2025-09-14 02:04:10 +0200

    New importing

--- Scripts/Components/LinkFolderComponent.cs
+++ Scripts/Components/LinkFolderComponent.cs
@@ -8,6 +8,10 @@ public partial class LinkFolderComponent : Node
     [Export] private HBoxContainer _thunmnailsBoxContainer;
     
     [Export] private PackedScene _photoThumbnailScene;
+
+    private int _currentImportedIndex = 0;
+
+    private Godot.Collections.Array<string> _pathsToImport = new();
     
 
 
@@ -31,43 +35,60 @@ public partial class LinkFolderComponent : Node
         
         _thunmnailsBoxContainer.GetChildren().Clear();
 
-        foreach (string path in paths)
-       {
-           GD.Print("Importing asset : " + path);
-
-           Image img = Image.LoadFromFile(path); 
-           
-           if (img == null || img.IsEmpty())
-           {
-               GD.PrintErr($"Failed to load image: {path}");
-               
-               continue;
-           }
-
-           Texture2D texture = ImageTexture.CreateFromImage(img);
-           
-           if (texture == null)
-           {
-               GD.PrintErr($"Failed to create texture from image: {path}");
-               
-               continue;
-           }
-
-           var thumbnail = _photoThumbnailScene.Instantiate<PhotoThumbnail>();
-           
-           if (thumbnail == null)
-           {
-               GD.PrintErr($"Failed to create thumbnail: {path}");
-
-               continue;
-           }
-           
-           
-           thumbnail.SetPhotoThumbnail(texture,path);
-           
-           _thunmnailsBoxContainer.AddChild(thumbnail);
-       }
+        _currentImportedIndex = 0;
         
+        _pathsToImport.Clear();
+
+        _pathsToImport = new Godot.Collections.Array<string>(paths);
         
+        ImportFiles();
+    }
+
+    private async void ImportFiles()
+    {
+        if (_pathsToImport != null && _currentImportedIndex >= 0 && _currentImportedIndex < _pathsToImport.Count)
+        {
+            string path = _pathsToImport[_currentImportedIndex];
+            
+            Image img = Image.LoadFromFile(path);
+            
+            if (img == null || img.IsEmpty())
+            {
+                GD.PrintErr($"Failed to load image: {path}");
+
+                return;
+            }
+
+            Texture2D texture = ImageTexture.CreateFromImage(img);
+
+            if (texture == null)
+            {
+                GD.PrintErr($"Failed to create texture from image: {path}");
+
+                return;
+            }
+
+            var thumbnail = _photoThumbnailScene.Instantiate<PhotoThumbnail>();
+
+            if (thumbnail == null)
+            {
+                GD.PrintErr($"Failed to create thumbnail: {path}");
+
+                return;
+            }
+
+            thumbnail.SetPhotoThumbnail(texture,path);
+
+            _thunmnailsBoxContainer.AddChild(thumbnail);
+
+            _currentImportedIndex++;
+            
+            await ToSignal(GetTree().CreateTimer(0.1), SceneTreeTimer.SignalName.Timeout);
+            
+            ImportFiles();
+
+
+        }
+
     }
 }
